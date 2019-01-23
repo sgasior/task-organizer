a5065658897645a0ba8925b7c1ad483449c04f53
Szymon <szgasior@outlook.com>
2019-01-23 20:51:08 +0100

    Initial commit

---
+++ src/test/java/pl/edu/kopalniakodu/todoapp/TodoAppApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.edu.kopalniakodu.todoapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TodoAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
