5f353f422d1daa78f7e82816a9519f5ebe840afa
Szymon <szgasior@outlook.com>
2019-01-23 21:17:58 +0100

    Added JPA annotattion to the task pojo & added Auditable class  & task class extented Auditable class

--- src/main/java/pl/edu/kopalniakodu/todoapp/domain/Task.java
+++ src/main/java/pl/edu/kopalniakodu/todoapp/domain/Task.java
@@ -2,15 +2,21 @@ package pl.edu.kopalniakodu.todoapp.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @RequiredArgsConstructor
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-@EqualsAndHashCode(of = "id")
-public class Task {
+@EqualsAndHashCode(of = "id", callSuper = false)
+public class Task extends Auditable {
 
-
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String title;
