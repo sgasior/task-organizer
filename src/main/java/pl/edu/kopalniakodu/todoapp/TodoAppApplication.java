ee3f408c8640c1d205dcbc788c0d3b764a5f082f
Szymon <szgasior@outlook.com>
2019-01-23 22:36:05 +0100

    #bugfix Added configuration annotation to get started Auditable class working

--- src/main/java/pl/edu/kopalniakodu/todoapp/TodoAppApplication.java
+++ src/main/java/pl/edu/kopalniakodu/todoapp/TodoAppApplication.java
@@ -2,8 +2,10 @@ package pl.edu.kopalniakodu.todoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TodoAppApplication {
 
     public static void main(String[] args) {
