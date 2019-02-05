f983fb6c951c23599eaaa8cafe4c2fe6aa1772cd
Szymon Gąsior <szgasior@outlook.com>
2019-02-05 13:04:12 +0100

    Typo in word MD_IMPORTANT (refactored) & and typo in mainPage

--- src/main/java/pl/edu/kopalniakodu/todoapp/domain/TaskWeight.java
+++ src/main/java/pl/edu/kopalniakodu/todoapp/domain/TaskWeight.java
@@ -3,7 +3,7 @@ package pl.edu.kopalniakodu.todoapp.domain;
 public enum TaskWeight {
 
     IMPORTANT("important"),
-    MD_IMPORANT("md_important"),
+    MD_IMPORTANT("md_important"),
     NOT_IMPORTANT("not_important");
 
 
--- src/main/java/pl/edu/kopalniakodu/todoapp/service/ScheduleService.java
+++ src/main/java/pl/edu/kopalniakodu/todoapp/service/ScheduleService.java
@@ -36,13 +36,13 @@ public class ScheduleService {
         taskList.add(new Task("Ruby programming to jest to", "Learn jUnit5, spring, hibernate Learn jUnit5, spring, hibernate" +
                 "Learn jUnit5, spring, hibernate" +
                 "Learn jUnit5, spring, hibernate hibernate hibernate", false, TaskWeight.IMPORTANT));
-        taskList.add(new Task("Exam", "Learn for exam which u have next Friday", true, TaskWeight.MD_IMPORANT));
+        taskList.add(new Task("Exam", "Learn for exam which u have next Friday", true, TaskWeight.MD_IMPORTANT));
         taskList.add(new Task("Shopping list", "Buy water, bread, ham", true, TaskWeight.NOT_IMPORTANT));
         taskList.add(new Task("Ruby programming", "Learn ruby on rails!", true, TaskWeight.IMPORTANT));
-        taskList.add(new Task("Csharp", "Download visual studio code!", true, TaskWeight.MD_IMPORANT));
+        taskList.add(new Task("Csharp", "Download visual studio code!", true, TaskWeight.MD_IMPORTANT));
         taskList.add(new Task("Phone dad", "Call dad and remind him about meeting", true, TaskWeight.NOT_IMPORTANT));
         taskList.add(new Task("SQL", "Write db for your project", true, TaskWeight.IMPORTANT));
-        taskList.add(new Task("Car", "remember to tank car! ", true, TaskWeight.MD_IMPORANT));
+        taskList.add(new Task("Car", "remember to tank car! ", true, TaskWeight.MD_IMPORTANT));
         taskList.add(new Task("Dentist", "Don't forget about dentist next monday", true, TaskWeight.NOT_IMPORTANT));
 
 
